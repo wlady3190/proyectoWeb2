10e487d4e952b5de56e43826efe6632712b61929
wlady3190 <wlady_3190@hotmail.com>
2023-05-10 22:09:53 -0500

    adding components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ManagementComponent } from './components/management/management.component';
+import { ProductsComponent } from './components/products/products.component';
+import { E404Component } from './components/e404/e404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'management', component:ManagementComponent},
+  {path:'products', component:ProductsComponent},
+  {path:'**', component:E404Component},
+  {path:'',  redirectTo:'home', pathMatch:'full'}
+  
+
+
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { SalesComponent } from './components/sales/sales.component';
+import { AboutComponent } from './components/about/about.component';
+import { ManagementComponent } from './components/management/management.component';
+import { E404Component } from './components/e404/e404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    SalesComponent,
+    AboutComponent,
+    ManagementComponent,
+    E404Component,
   ],
   imports: [
     BrowserModule,
