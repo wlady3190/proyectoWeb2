a669afeb8e61cedaec65f5ead33884ddf204caa8
wlady3190 <wlady_3190@hotmail.com>
2023-05-10 22:11:56 -0500

    adding components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import { HomeComponent } from './components/home/home.component';
 import { ManagementComponent } from './components/management/management.component';
 import { ProductsComponent } from './components/products/products.component';
 import { E404Component } from './components/e404/e404.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
   {path:'management', component:ManagementComponent},
   {path:'products', component:ProductsComponent},
+  {path:'about', component:AboutComponent},
   {path:'**', component:E404Component},
-  {path:'',  redirectTo:'home', pathMatch:'full'}
+  {path:'',  redirectTo:'home', pathMatch:'full'},
   
+
 
 
 
